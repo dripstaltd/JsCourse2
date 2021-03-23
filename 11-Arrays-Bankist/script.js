9c370bfbae9f333fcab12bc26118930766e555bc
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-23 09:05:23 +0000

    up

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -474,7 +474,7 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 console.log(movements);
 */
-
+/*
 const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
@@ -500,3 +500,61 @@ labelBalance.addEventListener('click', function () {
   );
   console.log(movementsUI);
 });
+*/
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+// Array Methods Practice
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2.
+const num1Deposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov >= 1000).length;
+
+console.log(num1Deposits1000);
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+
+console.log(numDeposits1000);
+
+//////////////////////////////////////////////
+//:Coding Challenge #4
+/*
+--------------------
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much 
+or too little.
+-
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+-
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+-----------
+Your tasks:
+-----------
+1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do not create a new array, simply loop over the array. Formula: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, 
+and the weight needs to be in kg)
+-
+2.   Find Sarah's dog and log to the console whether it's eating too much or too little. Hint: Some 
+dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is 
+a bit tricky (on purpose) 🤓
+-
+3.   Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an 
+array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4.   Log a string to the console for each array created in 3., like this: "Matilda and Alice and 
+Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5.   Log to the console whether there is any dog eating exactly the amount of food that is 
+recommended (just true or false)
+6.   Log to the console whether there is any dog eating an okay amount of food (just true or false)
+7.   Create an array containing the dogs that are eating an okay amount of food (try to reuse the 
+condition used in 6.)
+8.   Create a shallow copy of the 'dogs' array and sort it by recommended food portion in an 
+ascending order (keep in mind that the portions are inside the array's objects 😉
+*/
