3780fd2c846dde7db1278193280b142b099c164c
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-26 16:29:25 +0000

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -29,6 +29,29 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+/////////////////////////////////////////////////
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//     console.log('LINK');
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -38,7 +61,7 @@ document.addEventListener('keydown', function (e) {
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
-
+/*
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 
@@ -54,24 +77,26 @@ const message = document.createElement('div'); // creating a dom element
 message.classList.add('cookie-message');
 // message.textContent = 'sadas';
 message.innerHTML =
-  'We se cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+*/
 //-| Inserting elements --------------------------
 // header.prepend(message);
-header.append(message);
+// header.append(message);
 // header.append(message.cloneNode(true));
 // header.before(message);
 // header.after(message);
 
 //-| Delete elements -----------------------------
+/*
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
     // message.remove();
     message.parentElement.removeChild(message);
   });
-
+*/
 //-| Styles --------------------------------------
+/*
 message.style.backgroundColor = '#37383d';
 message.style.width = '105%';
 
@@ -81,16 +106,18 @@ message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
 document.documentElement.style.setProperty('--color-primary', 'orangered');
-
+*/
 //-| Attributes ----------------------------------
+/*
 const logo = document.querySelector('.nav__logo');
 // console.log(logo.alt);
 // console.log(logo.src);
 // console.log(logo.className);
 
 logo.alt = 'Beautiful minimalist logo with a panda';
-
+*/
 //-| Non-standard --------------------------------
+/*
 // console.log(logo.designer);
 // console.log(logo.getAttribute('designer'));
 logo.setAttribute('company', 'Bankist');
@@ -101,11 +128,12 @@ logo.setAttribute('company', 'Bankist');
 const link = document.querySelector('.nav__link--btn');
 // console.log(link.href);
 // console.log(link.getAttribute('href'));
-
+*/
 //-| Data attributes -----------------------------
 // console.log(logo.dataset.versionNumber);
 
 //-| Classes -------------------------------------
+/*
 logo.classList.add('c', 'j');
 logo.classList.remove('c', 'j');
 logo.classList.toggle('c');
@@ -113,9 +141,9 @@ logo.classList.contains('c'); // not includes
 
 //BUG THIS OVERRIDES!
 // logo.className = 'ben';
-
+*/
 //-| Smooth Scrolling-----------------------------
-
+/*
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
@@ -133,7 +161,7 @@ btnScrollTo.addEventListener('click', function (e) {
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
-*/
+
   // // Scrolling
   // window.scrollTo(
   //   s1coords.left + window.pageXOffset,
@@ -146,3 +174,49 @@ btnScrollTo.addEventListener('click', function (e) {
   //   behavior: 'smooth',
   // });
 });
+*/
+//-| Event Listener Types -------------------------
+const h1 = document.querySelector('h1');
+/*
+const alertH1 = function () {
+  alert('addEventListener1: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = alert(
+//   'addEventListener2: Great! You are reading the heading :D'
+// );
+*/
+
+//-| Event Propagation in Practice ----------------
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+/*
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopImmediatePropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  false
+);
+*/
