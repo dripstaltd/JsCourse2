076fc921f7331769c639c71b4b8018a4c193757a
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-28 13:32:52 +0000

    Menu fade animation
    Using bind method

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -9,6 +9,7 @@ const section1 = document.querySelector('#section--1');
 const tabs = document.querySelectorAll('.operations__tab'); // Buttons
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav'); // Navigation menu
 ///////////////////////////////////////
 //-| Modal window                     -
 ///////////////////////////////////////
@@ -73,10 +74,27 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+////////////////////////////////////////
+//-| Menu fade animation               -
+////////////////////////////////////////
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////////////
-////////////////////////////////////////
-////////////////////////////////////////
+//-| Sticky Navigation                 -
 ////////////////////////////////////////
 ////////////////////////////////////////
 ////////////////////////////////////////
