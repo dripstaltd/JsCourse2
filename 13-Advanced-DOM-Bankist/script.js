c1a325b69bc99a42ce5453799a9be39cd51583c4
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-28 14:22:31 +0000

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -10,6 +10,7 @@ const tabs = document.querySelectorAll('.operations__tab'); // Buttons
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav'); // Navigation menu
+
 ///////////////////////////////////////
 //-| Modal window                     -
 ///////////////////////////////////////
@@ -96,6 +97,16 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 ////////////////////////////////////////
 //-| Sticky Navigation                 -
 ////////////////////////////////////////
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 ////////////////////////////////////////
 ////////////////////////////////////////
 //-| Selecting elements                -
