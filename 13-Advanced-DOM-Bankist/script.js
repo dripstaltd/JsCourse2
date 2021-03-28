f7bb36c88596d823922ef918c448dabe2b2912b9
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-28 10:40:09 +0000

    Dom Traversing
    Dom Traversing: line-224

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -176,7 +176,7 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 */
 //-| Event Listener Types -------------------------
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 /*
 const alertH1 = function () {
   alert('addEventListener1: Great! You are reading the heading :D');
@@ -220,3 +220,33 @@ document.querySelector('.nav').addEventListener(
   false
 );
 */
+
+//-| Dom Traversing ------------------------------
+
+// Going downwards: child
+const h1 = document.querySelector('h1');
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); // Direct children
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// .closest / opposit of query selector
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
