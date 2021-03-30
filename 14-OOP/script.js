dddb78da324e006f28b19bf926cc4823adf2de73
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-30 10:28:41 +0000

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,4 +1,26 @@
 'use strict';
 //////////////////////////////////////////////////////////////////
-//-|  Object Oriented Programming                                ;
+//-|  Constructor Function + New Operator                        ;
 //////////////////////////////////////////////////////////////////
+/** 
+- Arrow functions do not work because we need our own THIS KEYWORD
+- Call constructor functions using 'new'
+
+// 1. New {} is created
+// 2. function's called |this keyword set to the newly created {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+*/
+//////////////////////////////////////////////////////////////////
+//>                                                              ;
+//////////////////////////////////////////////////////////////////
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const ben = new Person('Ben', 1991);
+const giselle = new Person('Giselle');
+
+console.log(ben);
