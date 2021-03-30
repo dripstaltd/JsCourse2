7b1713ea253bf2e449c4a7d28ff6554e381c64fb
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-30 09:43:01 +0000

    Object Oriented Programming
    # Abstraction
    # Encapsulation
    # Inheritance
    # Polymorphism

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,32 @@
 'use strict';
+//////////////////////////////////////////////////////////////////
+//-|                 Object Oriented Programming                 ;
+//////////////////////////////////////////////////////////////////
+//>  -  Abstraction:                                             ;
+/*     
+      Ignoring or hiding details that don't matter, allowing us
+      to get an OVERVIEW perspective of things we're implementing
+      instead of messing with details that don't really matter to 
+      our implementation 
+*/
+//>  -  Encapsulation:                                           ;
+/*
+      Keeping properties and methods PRIVATE inside the class, so
+      they are NOT ACCESSIBLE FROM OUTSIDE THE CLASS. Some methods
+      can be exposed as a public interface (API).
+*/
+//>  -  Inheritance                                              ;
+/*
+      Making all properties and methods of a class available to a
+      child class, that forms a hierarchical relationship between
+      classes. This allows us to reuse common logic and to model
+      real-world relationships.
+*/
+//>  -  Polymorphism                                             ;
+/*
+        A child class can overwrite a method it inherited from a
+        parent class [ it's more complex than this ].
+*/
+//////////////////////////////////////////////////////////////////
+//>                                                              ;
+//////////////////////////////////////////////////////////////////
