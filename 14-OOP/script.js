1e2925f1205b1fecb3fb1abfda755efe80cbedcc
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-30 13:47:29 +0000

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -105,32 +105,68 @@ console.dir(x => x + 1);
 const PersonCl = class {};
 */
 //> Class declaration
-/*
+
 class PersonCl {
   constructor(firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
   }
+
   // Methods will be added .prototype property
   calcAge() {
     console.log(2021 - this.birthYear);
   }
-  greet = function () {
-    console.log(`Hey ${this.firstName}`);
-  };
-}
 
-const jessica = new PersonCl('Jessica', 1996);
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2021 - this.birthYear;
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
-
-// PersonCl.prototype.greet = function () {
-//   console.log(`Hey ${this.firstName}`);
-// };
-
+console.log(jessica.age);
 jessica.greet();
-*/
+
+const walter = new PersonCl('Walter', 1965);
 
 //////////////////////////////////////////////////////////////////
-//-| SETTERS & GETTERS - PROPERTIES                              ;
+//-| SETTERS & GETTERS - DATA PROPERTIES                         ;
+//////////////////////////////////////////////////////////////////
+//> Functions that get or set a property                         ;
+//////////////////////////////////////////////////////////////////
+const account = {
+  owner: 'ben',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+console.log(jessica.age);
+
+//////////////////////////////////////////////////////////////////
+//-| STATIC METHODS                                              ;
 //////////////////////////////////////////////////////////////////
