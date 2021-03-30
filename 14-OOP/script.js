d8cdc0a79c1cf20c04d3c31f6ebe75f0cb2d190e
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-30 10:55:54 +0000

    Properties and Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -5,6 +5,7 @@
 /** 
 - Arrow functions do not work because we need our own THIS KEYWORD
 - Call constructor functions using 'new'
+- Capitalize the name of the {Instance} 
 
 // 1. New {} is created
 // 2. function's called |this keyword set to the newly created {}
@@ -16,11 +17,39 @@
 //////////////////////////////////////////////////////////////////
 
 const Person = function (firstName, birthYear) {
+  // Instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
 };
 
 const ben = new Person('Ben', 1991);
-const giselle = new Person('Giselle');
+const giselle = new Person('Giselle', 1960);
+const lewis = new Person('Lewis', 1992);
 
-console.log(ben);
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+ben.calcAge();
+giselle.calcAge();
+lewis.calcAge();
+
+console.log(Person.prototype);
+console.log(ben.__proto__);
+console.log(ben.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(giselle));
+// Honest name: Prototype of linked objects
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(ben.species, lewis.species);
+
+/*
+    This shows that 'firstName' is a property of 'ben'
+*/
+console.log(ben.hasOwnProperty('firstName'));
+/* 
+    Species is not a property of 'ben' because it is not really inside the ben object. However it has access to it because it's in the prototype property    of person.
+*/
+console.log(ben.hasOwnProperty('species'));
