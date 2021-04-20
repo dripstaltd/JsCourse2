733a70c05152d85e4f1f3d0e786ce33309987bc1
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-04-20 14:57:14 +0000

    using GIT bash and Npm installer to manage packages

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -56,3 +56,21 @@ const { addTocart } = require('./shoppingCart.js');
 */
 
 // -| Command Line ;
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+console.log(stateClone);
+state.user.loggedIn = false;
+
+console.log(stateDeepClone);
